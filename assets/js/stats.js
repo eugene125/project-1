aa2d048d5e673faf08eb1d159597c1428091f534
Craig Leslie <cleslie25@gmail.com>
2022-03-22 10:16:33 -0500

    Centered team logo, division, and conference

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -32,16 +32,19 @@ function teamHeader(teamSpecific, conferencePg2, divisionPg2){
     //creates h1 content
     let teamHeader = document.createElement('h1');
     teamHeader.textContent = teamSpecific;
+    teamHeader.classList.add('center')
 
     //creates ul
     let teamUl = document.createElement('ul');
+    teamUl.classList.add('center')
+    teamUl.classList.add('left')
 
     //creates Li
-    let teamLi1 = document.createElement('li');
+    let teamLi1 = document.createElement('p');
     teamLi1.textContent = `Conference: ${conferencePg2}`;
 
     //creates Li
-    let teamLi2 = document.createElement('li');
+    let teamLi2 = document.createElement('p');
     teamLi2.textContent = `Division: ${divisionPg2}`;
 
     //appends h1
